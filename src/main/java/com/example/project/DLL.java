182671e18f7b9201161b967d35e90969e0397052
Yasir-Alfaifi <aggee12345@gmail.com>
2024-11-19 12:03:18 +0300

    Update DLL.java

--- src/main/java/com/example/project/DLL.java
+++ src/main/java/com/example/project/DLL.java
@@ -1,102 +1,119 @@
 package com.example.project;
 
+class DLLNode<T> {
+    T data;
+    DLLNode<T> next;
+    DLLNode<T> previous;
+
+    public DLLNode(T val) {
+        data = val;
+        next = previous = null;
+    }
+}
+
 public class DLL<T> {
-	private DLLNode<T> head;
-	private DLLNode<T> current;
+    private DLLNode<T> head;
+    private DLLNode<T> current;
 
     public DLL() {
         head = current = null;
     }
+
     public boolean empty() {
         return head == null;
     }
+
     public boolean last() {
         return current.next == null;
     }
+
     public boolean first() {
         return current.previous == null;
     }
+
     public boolean full() {
         return false;
     }
+
     public void findFirst() {
         current = head;
     }
+
     public void findNext() {
-        current = current.next;
+        if (current != null)
+            current = current.next;
     }
+
     public void findPrevious() {
-        current = current.previous;
+        if (current != null)
+            current = current.previous;
     }
+
     public T retrieve() {
-        return current.data;
+        return current != null ? current.data : null;
     }
+
     public void update(T val) {
-        current.data = val;
+        if (current != null)
+            current.data = val;
     }
+
     public void insert(T val) {
-        DLLNode<T> tmp = new DLLNode<T>(val);
-        if(empty()) {
+        DLLNode<T> tmp = new DLLNode<>(val);
+        if (empty()) {
             current = head = tmp;
-        }
-        else {
+        } else {
             tmp.next = current.next;
             tmp.previous = current;
-            if(current.next != null)
+            if (current.next != null)
                 current.next.previous = tmp;
             current.next = tmp;
             current = tmp;
         }
     }
+
     public void remove() {
-        if(current == head) {
+        if (current == null) return;
+
+        if (current == head) {
             head = head.next;
-            if(head != null)
-               head.previous = null;
-        }
-        else {
+            if (head != null)
+                head.previous = null;
+        } else {
             current.previous.next = current.next;
-            if(current.next != null)
-               current.next.previous = current.previous;
+            if (current.next != null)
+                current.next.previous = current.previous;
         }
-        if(current.next == null)
-            current = head;
-        else
-            current = current.next;
+        current = (current.next == null) ? head : current.next;
     }
-    public void removeBetween(T e1, T e2) {  
-    	DLLNode<T> e1Node = null;
-    	DLLNode<T> e2Node = null;
-    	
-    	current = head;
-    	
-    	while(current != null) {
-    		if(current.data.equals(e1)) {
-    			e1Node = current;
-    			current = current.next;
-    			break;
-    		}
-    		else if(current.next == null)
-    			return;
-    		
-    		current = current.next;
-    	}
-    	
-    	while(current != null) {
-    		if(current.data.equals(e2)) {
-    			e2Node = current;
-    			break;
-    		}
-    		else if(current.next == null)
-    			return;
-    		
-    		current = current.next;
-    	}
-    	if(e1Node != null && e2Node != null) {
-    		e1Node.next = e2Node;
-    		e2Node.previous = e1Node;
-    	}
-    
-    	current = head;
+
+    public void removeBetween(T e1, T e2) {
+        DLLNode<T> e1Node = null;
+        DLLNode<T> e2Node = null;
+
+        current = head;
+        while (current != null) {
+            if (current.data.equals(e1)) {
+                e1Node = current;
+                break;
+            }
+            current = current.next;
+        }
+
+        if (e1Node == null) return;
+
+        current = e1Node.next;
+        while (current != null) {
+            if (current.data.equals(e2)) {
+                e2Node = current;
+                break;
+            }
+            current = current.next;
+        }
+
+        if (e2Node == null) return;
+
+        e1Node.next = e2Node;
+        e2Node.previous = e1Node;
     }
 }
